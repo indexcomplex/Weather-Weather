afe21d717ffbed3c1a752759e185382cb1ab18da
alan hernandez <indexcomplex@gmail.com>
2019-07-01 13:48:38 -0700

    Have buttons no logic

---
+++ Weather Weather/Weather Weather/WeatherVC.swift
@@ -0,0 +1,36 @@
+//
+//  WeatherVC.swift
+//  Weather Weather
+//
+//  Created by Alan Hernandez on 6/30/19.
+//  Copyright © 2019 alan Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    
+    
+    @IBOutlet weak var cityLabel:UILabel! 
+    @IBOutlet weak var weatherImage:UIImageView!
+    @IBOutlet weak var temperatureLabel:UILabel!
+    @IBOutlet weak var descriptionLabel:UILabel!
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+
+       
+    }
+    
+
+    
+
+}
